cbe2fe071630ca7a752a7a1899c893f62600f092
Jerry Chen <jche318@gmail.com>
2019-07-22 14:54:07 +1000

    updated with arguments and fruit replacements

--- Notes_cleanser_v1.py
+++ Notes_cleanser_v1.py
@@ -1,25 +1,28 @@
 import pandas as pd
+import sys
+import random
 
 # Parameters
-fileName = "Analysis_Latest_Test"
+fileName = sys.argv[1]
 sheetName = "Sheet1"
 colName = "Note"
 
 # Read in excel file and first/last name databases
-df = pd.read_excel(fileName+".xlsx",sheet_name=sheetName)
+df = pd.read_excel(fileName,sheet_name=sheetName)
 firstNames = pd.read_csv("FirstNameDatabase.csv")
 lastNames = pd.read_csv("LastNameDatabase.csv")
+names = ["Apple","Apricot","Avocado","Banana","Berry","Cantaloupe","Cherry","Citron","Citrus","Coconut","Date","Fig","Grape","Guava","Kiwi","Lemon","Lime","Mango","Melon","Mulberry","Nectarine","Orange","Papaya","Peach","Pear","Pineapple","Plum","Prune","Raisin","Raspberry","Tangerine"]
 
 # Iterate over each word in each row and match against databases
 for idx,row in df.iterrows():
     tokens = row[colName].split()
     for i in range(len(tokens)):
         if tokens[i].lower().capitalize() in firstNames.values:
-            tokens[i] = "Joe"
+            tokens[i] = random.choice(names)
         elif tokens[i].lower().capitalize() in lastNames.values:
-            tokens[i] = "Bloggs"
+            tokens[i] = random.choice(names)
     df.at[idx,colName] = " ".join(tokens)
     print(str(idx + 1) + " completed.")
 
 # Write dataframe to csv
-df.to_csv(fileName+"_cleansed.csv",index=False)
+df.to_csv("output.csv",index=False)
